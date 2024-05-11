fa848c08c6ec152a9985c6cab7e020a30c21ba2d
courageluo <courageluo@gmail.com>
2024-05-12 00:07:45 +0800

    fix problems

---
+++ run.py
@@ -0,0 +1,7 @@
+import http.server
+import socketserver
+
+Handler = http.server.SimpleHTTPRequestHandler
+with socketserver.TCPServer(("", 8000), Handler) as httpd:
+ print("serving at port", 8000)
+ httpd.serve_forever()

258e0fb11cab918c5867785c61de70ba0138215c
courageluo <courageluo@gmail.com>
2025-01-16 16:29:55 +0800

    Updated blog

--- blog/content/0009.idianjiao-script/script-old.js
+++ blog/content/0009.idianjiao-script/script-old.js
@@ -2,11 +2,11 @@
 // @name         自动显示听力答案
 // @namespace    https://courageluo.xn--kivy66br3pu7d.top/
 // @supportURL   courageluo@gmail.com
-// @version      2025-01-16
+// @version      2024-07-30
 // @description  Display the answer of idianjiao automatically
 // @author       Courage Luo
 // @match        https://www.idianjiao.com/*
-// @icon         https://courageluo.xn--kivy66br3pu7d.top/img/favicon.svg
+// @icon         https://courageluo.xn--kivy66br3pu7d.top/img/favicon.png
 // @grant        none
 // ==/UserScript==
 
@@ -14,51 +14,39 @@
     'use strict';
 
     window.addEventListener('load', function() {
-        // 创建提示文本
-        var skipTip = document.createElement('p');
-        skipTip.innerText = '\n请点击 "跳过" 按钮以确保脚本正常加载';
-        skipTip.style.color = '#c4a699';
+        // 给你2.5秒按"跳过"按钮，原因见下
+        setTimeout(() => {
+            // 大多数现代浏览器为了防止自动播放广告和其他不必要的声音，限制了自动播放音频的行为。
+            // 浏览器要求用户必须有与页面的交互（如点击、键盘输入等），然后才能播放音频。
+            // 所以自动按按钮不会播放音频，也就按不了提交按钮，所以懒得折腾了
+            // var btn = document.querySelector('.tg');
+            // btn.click();
 
-        var bsTip = document.getElementsByClassName('bsTip')[0];
-        bsTip.appendChild(skipTip);
-
-        // 等待按钮按下后执行
-        var tgButton = document.querySelector('.tg');
-        tgButton.addEventListener('click', function() {
-            applyDisplay();
-        });
-
-        // 主要内容
-        function applyDisplay() {
-            // 获取元素数量
-            const numElements = document.querySelectorAll('.answer').length;
-
-            // 获取答案并存储
+            // 获取答案并存储在数组中
             var answers = new Array();
-            for (var i = 0; i < numElements; i ++ ) {
+            for (var i = 0; i < 20; i ++ ) {
                 var ans = document.querySelectorAll('.incorrect')[i].innerText.replace('回答错误,正确答案', '');
                 answers[i] = '第' + (i + 1) + '题选择' + ans + '项';
             }
 
-            // 获取题目元素
+            // 获取元素
             var questions = document.querySelectorAll('.answer');
 
             // 确保数量匹配
-            if (questions.length !== numElements) {
+            if (questions.length !== answers.length) {
                 console.error('数量不匹配！');
                 return;
             }
 
-            // 遍历 questions 并插入对应的 answer
+            // 遍历questions并插入对应的answer
             for (i = 0; i < questions.length; i++) {
                 // 创建元素存放答案
                 var answer = document.createElement('p');
-                answer.innerText = answers[i];
+                answer.textContent = answers[i];
 
                 // 调节一下样式
-                answer.style.marginTop = '5px';
                 answer.style.fontSize = '14px';
-                answer.style.color = '#d85b2f';
+                answer.style.color = '#FF3333';
 
                 // 获取当前元素
                 var question = questions[i];
@@ -66,6 +54,6 @@
                 // 在当前元素之后插入答案元素
                 question.parentNode.insertBefore(answer, question.nextSibling);
             }
-        }
+        }, 2500);
     });
 })();
